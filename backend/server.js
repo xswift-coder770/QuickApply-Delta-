e69fab73e78514eed0029992bc2f898b9238c3fe
xswift-coder770 <155413497+xswift-coder770@users.noreply.github.com>
2025-10-02 12:33:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
+ 
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
