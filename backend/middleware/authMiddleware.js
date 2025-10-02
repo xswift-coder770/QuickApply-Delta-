f7b59a309f5d6a50c627f24910fa08f01f8aa58e
xswift-coder770 <155413497+xswift-coder770@users.noreply.github.com>
2025-10-02 12:32:39 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -18,7 +18,7 @@ const auth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
-    // ✅ Fixed: store full decoded payload (not just id)
+ 
     req.user = decoded;
 
     next();
