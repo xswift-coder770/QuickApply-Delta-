1040cecafa1e9199ffba413bac57e6e05063980f
xswift-coder770 <155413497+xswift-coder770@users.noreply.github.com>
2025-10-18 16:15:03 +0530

    Update aiRoutes.js

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,14 +1,14 @@
- 
 
-//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// backend/routes/aiRoutes.js
+
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
-const { saveRephrased, deleteRephrased, rephraseText } = require("../controllers/aiController");
-
-router.post("/save", auth, saveRephrased);
-
-router.delete("/delete/:id", auth, deleteRephrased);
+const { rephraseText, saveRephrased, deleteRephrased } = require("../controllers/aiController");
+ 
 router.post("/rephrase", rephraseText);
+ 
+router.post("/save", auth, saveRephrased);
+router.delete("/delete/:id", auth, deleteRephrased);
 
 module.exports = router;
